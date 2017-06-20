f19c955d786bbb1ca59309acf1d7d90984b25f44
wuhang <15733173176@163.com>
2017-06-20 16:08:09 +0800

    scand use
    scand use

---
+++ stu/modsc.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+
+	char sdept[32] = "\0";
+	char major[32] = "\0";
+	int status = 0;
+
+	status = cgiFormString("sdept",  sdept, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sdept error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("major",  major, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get major error!\n");
+		return 1;
+	}
+	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+	sprintf(sql, "update school set major='%s'  where sdept='%s' ", major, sdept);
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+
+	fprintf(cgiOut, "update college ok!\n");
+	mysql_close(db);
+	return 0;
+}
