f499ce44d0c5ca6cf30bc0c20874732c1c14d69c
wuhang <15733173176@163.com>
2017-06-20 10:35:38 +0800

    cenggai

---
+++ stu/add.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+	char name[32] = "\0";
+	char age[16] = "\0";
+	char stuId[32] = "\0";
+  char sex[32] = "\0";
+	char sdept[32] = "\0";
+	int status = 0;
+
+	status = cgiFormString("name",  name, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get name error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("age",  age, 16);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get age error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("stuId",  stuId, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get stuId error!\n");
+		return 1;
+	}
+	status = cgiFormString("sex",  sex, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sex error!\n");
+		return 1;
+	}
+	status = cgiFormString("sdept",  sdept, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sdept error!\n");
+		return 1;
+	}
+
+	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+
+	strcpy(sql, "create table stu(id char(20) not null primary key, name varchar(20) not null, age int not null, sex varchar(20) not null, sdept int not null)");
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		if (ret != 1)
+		{
+			fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+			mysql_close(db);
+			return -1;
+		}
+	}
+
+	sprintf(sql, "insert into stu values(%s, '%s', '%d', '%s', %d)", stuId, name, atoi(age), sex, atoi(sdept));
+	if (mysql_real_query(db, sql, strlen(sql) + 1) != 0)
+	{
+		fprintf(cgiOut, "%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+	fprintf(cgiOut, "add student ok!\n");
+	mysql_close(db);
+	return 0;
+}
